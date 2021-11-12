8389054bfd5fb73f00ae1035d9252cda9b3f463c
AdamField7 <89855015+AdamField7@users.noreply.github.com>
2021-11-11 18:40:45 -0600

    updated script

--- Assets/Scripts/LookAtPlayer.cs
+++ Assets/Scripts/LookAtPlayer.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class LookAtPlayer : MonoBehaviour
 {
-    public GameObject camera;
+
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.LookAt(camera.transform);
+        gameObject.transform.LookAt(Camera.main.transform);
         gameObject.transform.eulerAngles = new Vector3(0, gameObject.transform.eulerAngles.y, 0);
         gameObject.transform.Rotate(Vector3.up, 180);
     }
